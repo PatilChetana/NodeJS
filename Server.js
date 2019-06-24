5cb3b172febd242b4ddb76c045d8a064bf7670a8
is_chetanapatil <chetana.patil@infostretch.com>
2019-06-24 16:07:47 +0530

    'Heroku'

--- Server.js
+++ Server.js
@@ -1,6 +1,7 @@
 const express=require('express');
 const hbs=require('hbs');
 
+const port=process.env.PORT || 5000;
 var app=express();
 
 
@@ -55,4 +56,4 @@ app.get('/about',(req,res)=>
 });
 
 
-app.listen(5000);
+app.listen(port);
